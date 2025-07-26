a241c2e7c80354d7441ce5b578146822d3c6dfbd
Eyobed <eyobedteshome@gmail.com>
2025-07-27 01:25:18 +0300

    models.py updated

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,20 @@
+from relationship_app.models import Author, Book, Library;
+
+# Query all books by a specific author
+books = Book.objects.filter(author__name="John Doe")
+print(books)
+
+# List all books in a library
+library = Library.objects.get(id=1) 
+books = library.books.all()
+print(books)
+
+# Retrieve the librarian for a library
+librarian = library.librarian
+print(librarian)
+
+# Create a new book
+adam = Author(name="adam")
+adam.save()
+new_book = Book(title='Genesis', author=adam)
+new_book.save()
