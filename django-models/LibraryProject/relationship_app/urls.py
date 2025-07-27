bfac22ec75fdfe30ff061f47ec8e02eb89ac1016
Eyobed <eyobedteshome@gmail.com>
2025-07-27 23:42:15 +0300

    done authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,4 +4,7 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path("books/", list_books, name="list-books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-detail"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("register/", user_register, name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,3 +13,36 @@ class LibraryDetailView(DetailView):
     context_object_name = "library"
 # relationship_app/list_books.html", "Book.objects.all()"]
 # "relationship_app/library_detail.html", "from .models import Library"
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("list-books")  # or any homepage
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+
+def user_register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
