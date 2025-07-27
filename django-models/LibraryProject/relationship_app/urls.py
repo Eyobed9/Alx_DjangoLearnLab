cf476ee5daadd6cc6f1faee7be720b9b1e0700d3
Eyobed <eyobedteshome@gmail.com>
2025-07-27 23:52:46 +0300

    last one

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,3 +15,4 @@ urlpatterns = [
     path("books/delete/<int:pk>/", views.delete_book, name="delete_book"),
 ]
 # LibraryProject/relationship_app/urls.py doesn't contain: ["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
+# "add_book/", "edit_book/"]
