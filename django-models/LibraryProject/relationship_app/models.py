2a70f3bf52cbda586e7b6eb1207f177c0283b028
Eyobed <eyobedteshome@gmail.com>
2025-07-27 23:52:22 +0300

    last one

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -37,3 +37,22 @@ class Librarian(models.Model):
 
 # # LibraryProject/relationship_app/urls.py doesn't contain: ["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
 #LibraryProject/relationship_app/models.py doesn't contain: ["class UserProfile(models.Model):", "Admin", "Member"]
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
     path("register/", user_register, name="register"),
-        path("admin-role/", admin_view, name="admin_view"),
+    path("admin-role/", admin_view, name="admin_view"),
     path("librarian-role/", librarian_view, name="librarian_view"),
     path("member-role/", member_view, name="member_view"),
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/edit/<int:pk>/", views.edit_book, name="edit_book"),
+    path("books/delete/<int:pk>/", views.delete_book, name="delete_book"),
 ]
 # LibraryProject/relationship_app/urls.py doesn't contain: ["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
