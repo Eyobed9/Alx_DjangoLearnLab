927cf9b57d02ddf1f9c952fe95a824309e96d22e
Eyobed <eyobedteshome@gmail.com>
2025-08-10 23:46:18 +0300

    views impelemented

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,15 +1,33 @@
 from rest_framework import generics
+from rest_framework import permissions
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import permissions
 
-class BookList(generics.ListCreateAPIView):
+class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Allow read for all, write for authenticated users
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Allow read for all, write for authenticated users
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
